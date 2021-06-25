d8db8e5d2973bfe8e8b28dc92c57417b8fb01966
Rajendra Indukuri <Rajendra.Indukuri@emc.com>
2021-06-25 17:54:27 +0530

    RTS v1.2.0

---
+++ plugins/doc_fragments/dellemc_powerscale.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Dell EMC.
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+
+    DOCUMENTATION = r'''
+options:
+  - See respective platform section for more details
+requirements:
+  - See respective platform section for more details
+notes:
+  - Ansible modules are available for Dell EMC PowerScale Storage Platform
+'''
+    # Documentation fragment for PowerScale (powerscale)
+    POWERSCALE = r'''
+    options:
+        onefs_host:
+            description:
+            - IP address or FQDN of the PowerScale cluster.
+            type: str
+            required: True
+        port_no:
+            description:
+            - Port number of the PowerScale cluster.It defaults to 8080 if
+              not specified.
+            type: str
+            required: False
+            default: '8080'
+        verify_ssl:
+            description:
+            - boolean variable to specify whether to validate SSL
+              certificate or not.
+            - True - indicates that the SSL certificate should be
+              verified.
+            - False - indicates that the SSL certificate should not be
+              verified.
+            type: bool
+            required: True
+            choices: [True, False]
+        api_user:
+            type: str
+            description:
+            - username of the PowerScale cluster.
+            required: True
+        api_password:
+            type: str
+            description:
+            - the password of the PowerScale cluster.
+            required: True
+    requirements:
+      - A DellEMC PowerScale Storage system.
+      - Ansible 2.7 and above.
+    notes:
+      - The modules prefixed with dellemc_powerscale are built to support the
+        DellEMC PowerScale storage platform.
+    '''
