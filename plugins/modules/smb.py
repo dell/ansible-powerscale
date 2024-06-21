addf006a6254d2b9359376643e1bc84ca8742cf1
sharmb5 <bhavneet.sharma@dell.com>
2024-06-21 06:18:20 -0400

    Adding SMB enhacements

--- plugins/modules/smb.py
+++ plugins/modules/smb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright: (c) 2019, Dell Technologies
+# Copyright: (c) 2019-2024, Dell Technologies
 
 # Apache License version 2.0 (see MODULE-LICENSE or http://www.apache.org/licenses/LICENSE-2.0.txt)
 
