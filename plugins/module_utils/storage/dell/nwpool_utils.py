5e7895463ef355e5edaa486c27d0480fd96a736f
Datta <trisha_datta@dell.com>
2024-08-08 16:26:24 +0530

    Adding fix for network pool module for 9.7 and 9.8

--- plugins/module_utils/storage/dell/nwpool_utils.py
+++ plugins/module_utils/storage/dell/nwpool_utils.py
@@ -30,14 +30,9 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import json
-import re
-import time
-import os
 from ansible.module_utils.urls import open_url, ConnectionError, SSLValidationError
 from ansible.module_utils.six.moves.urllib.error import URLError, HTTPError
 from ansible.module_utils.six.moves.urllib.parse import urlencode
-from ansible.module_utils.common.parameters import env_fallback
-from ansible.module_utils.basic import AnsibleModule
 
 
 class OpenURLResponse(object):
