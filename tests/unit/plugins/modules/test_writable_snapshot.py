aa4a0f54643f094aa0f2abfcdac78342a2ed1640
Kritika-Bhateja-03 <Kritika.Bhateja@Dell.com>
2024-08-13 13:04:31 +0000

    UT

--- tests/unit/plugins/modules/test_writable_snapshot.py
+++ tests/unit/plugins/modules/test_writable_snapshot.py
@@ -10,24 +10,19 @@ __metaclass__ = type
 
 import pytest
 from mock.mock import MagicMock
-# pylint: disable=unused-import
 from ansible_collections.dellemc.powerscale.tests.unit.plugins.module_utils.shared_library.initial_mock \
     import utils
 from ansible_collections.dellemc.powerscale.plugins.modules.writable_snapshot import WritableSnapshot, WritableSnapshotHandler
-# from ansible_collections.dellemc.powerscale.tests.unit.plugins.module_utils.mock_alert_management_api import MockAlertRuleApi
-from ansible_collections.dellemc.powerscale.tests.unit.plugins.module_utils.mock_api_exception \
-    import MockApiException
 from ansible_collections.dellemc.powerscale.tests.unit.plugins.module_utils.shared_library.powerscale_unit_base \
     import PowerScaleUnitBase
 
 
 class TestWritableSnapshot(PowerScaleUnitBase):
-
     writable_snapshot_args = {"onefs_host": "XX.XX.XX.XX",
                               "port_no": "8080",
                               "verify_ssl": "false",
                               "api_user": "root",
-                              "api_password": "pancake"
+                              "api_password": '***'
                               }
 
     @pytest.fixture
@@ -36,7 +31,7 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_create_writable_snapshot_check_mode(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "present"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible1/", "state": "present"}]})
         powerscale_module_mock.check_mode = True
         powerscale_module_mock.segregate_snapshots = MagicMock(
             return_value=([{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "present"}], [], []))
@@ -46,7 +41,7 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_create_writable_snapshot(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "present"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible2/", "state": "present"}]})
         powerscale_module_mock.check_mode = False
         powerscale_module_mock.get_writable_snapshot = MagicMock(return_value=(False, []))
         WritableSnapshotHandler().handle(powerscale_module_mock, powerscale_module_mock.module.params)
@@ -54,7 +49,7 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_create_writable_snapshot_idempotence_mode(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "present"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible3/", "state": "present"}]})
         powerscale_module_mock.check_mode = False
         powerscale_module_mock.get_writable_snapshot = MagicMock(
             return_value=(True, [{"src_snap": 2, "dst_path": "/ifs/ansible/"}]))
@@ -63,16 +58,16 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_create_writable_snapshot_exception(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "present"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible4/", "state": "present"}]})
         powerscale_module_mock.check_mode = False
         powerscale_module_mock.determine_error = MagicMock()
         powerscale_module_mock.get_writable_snapshot = MagicMock(return_value=(False, []))
-        powerscale_module_mock.snapshot_api.create_snapshot_writable_item = MagicMock(side_effect=MockApiException)
+        powerscale_module_mock.snapshot_api.create_snapshot_writable_item = MagicMock(side_effect=utils.ApiException)
         self.capture_fail_json_call("Failed to create writable snapshot", powerscale_module_mock, WritableSnapshotHandler)
 
     def test_delete_writable_snapshot_check_mode(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "absent"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible5/", "state": "absent"}]})
         powerscale_module_mock.check_mode = True
         powerscale_module_mock.get_writable_snapshot = MagicMock(return_value=(True, []))
         WritableSnapshotHandler().handle(powerscale_module_mock, powerscale_module_mock.module.params)
@@ -80,7 +75,7 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_delete_writable_snapshot(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "absent"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible6/", "state": "absent"}]})
         powerscale_module_mock.check_mode = False
         powerscale_module_mock.get_writable_snapshot = MagicMock(
             return_value=(True, [{"src_snap": 2, "dst_path": "/ifs/ansible/"}]))
@@ -89,7 +84,7 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_delete_writable_snapshot_idempotence_mode(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "absent"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible7/", "state": "absent"}]})
         powerscale_module_mock.check_mode = False
         powerscale_module_mock.get_writable_snapshot = MagicMock(return_value=(False, []))
         WritableSnapshotHandler().handle(powerscale_module_mock, powerscale_module_mock.module.params)
@@ -97,7 +92,7 @@ class TestWritableSnapshot(PowerScaleUnitBase):
 
     def test_delete_writable_snapshot_exception(self, powerscale_module_mock):
         self.set_module_params(powerscale_module_mock, self.writable_snapshot_args,
-                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible/", "state": "absent"}]})
+                               {"writable_snapshot": [{"src_snap": 2, "dst_path": "/ifs/ansible8/", "state": "absent"}]})
         powerscale_module_mock.check_mode = False
         powerscale_module_mock.snapshot_api.delete_snapshot_writable_wspath = MagicMock(side_effect=Exception)
-        self.capture_fail_json_call("Failed to delete snapshot: /ifs/ansible/ with error", powerscale_module_mock, WritableSnapshotHandler)
+        self.capture_fail_json_call("Failed to delete snapshot: /ifs/ansible8/ with error", powerscale_module_mock, WritableSnapshotHandler)
