f6f8d78efa278da5910c6e3480dc8bacbcc8ebed
Shivam Sharma <Shivam.sharma3@dell.com>
2025-01-21 05:52:54 -0500

    doc change

--- tests/unit/plugins/module_utils/mock_ldap_api.py
+++ tests/unit/plugins/module_utils/mock_ldap_api.py
@@ -2,7 +2,7 @@
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-"""Mock API responses for PowerScale Network Rule module"""
+"""Mock API responses for PowerScale LDAP module"""
 
 from __future__ import (absolute_import, division, print_function)
 
