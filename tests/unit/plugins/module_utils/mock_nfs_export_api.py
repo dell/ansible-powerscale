01c0cfb2a9287d02b76aef4995e3d2f1b23412ec
Fabian Brauner <54020707+fpfuetsch@users.noreply.github.com>
2026-03-30 10:43:27 +0200

    Add support for NFS export advanced settings (#214)
    Signed-off-by: fpfuetsch <54020707+fpfuetsch@users.noreply.github.com>
    Co-authored-by: Saksham-Nautiyal <167847889+Saksham-Nautiyal@users.noreply.github.com>
    Co-authored-by: Aniket Das <aniket.das@dell.com>

--- tests/unit/plugins/module_utils/mock_nfs_export_api.py
+++ tests/unit/plugins/module_utils/mock_nfs_export_api.py
@@ -87,6 +87,12 @@ NFS_1 = {"exports": [{
             "type": None
         }
     },
+    "map_failure": {
+        "enabled": False,
+        "primary_group": {},
+        "secondary_groups": [],
+        "user": {},
+    },
     "snapshot": None,
     "zone": SYS_ZONE}]}
 
